faaef57feadfb815e17f868d888478d489d45297
azraelite <33286699+azraelite@users.noreply.github.com>
2018-03-12 18:14:13 +0100

    Add files via upload

---
+++ Morse/Morse.h
@@ -0,0 +1,48 @@
+#ifndef Morse_h
+#define Morse_h
+#if ARDUINO>=100
+	#include "Arduino.h"
+	#else 
+		#include "WProgram.h"
+ #endif
+
+class Morse {
+	public :
+	Morse();
+	void moe();
+	void moi();
+	void mos();
+	void moh();
+	void mo5();
+
+	void test();
+	void setupMorse(uint8_t pin, long time);
+	
+	
+	private :
+	
+	uint8_t _pin;
+	long _time_unit;
+	long _dot;
+	long _dash;
+	long _igap;
+	long _sgap;
+	long _mgap;
+	unsigned long _time_now;
+	void signal_m();
+	void signal_o();
+	void signal_e();
+	void signal_i();
+	void signal_s();
+	void signal_h();
+	void signal_5();
+	void signal_short_gap();
+	
+	
+	
+	
+};
+
+#endif
+
+	
